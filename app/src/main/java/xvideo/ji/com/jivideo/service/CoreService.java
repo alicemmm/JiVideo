fabe27423a7a98f9f50f6781d4a509b8f1302315
alicemmm <YinJim@YinJims-MacBook-Air-2.local>
2015-10-16 17:50:09 +0800

    modify service

--- app/src/main/java/xvideo/ji/com/jivideo/service/CoreService.java
+++ app/src/main/java/xvideo/ji/com/jivideo/service/CoreService.java
@@ -76,7 +76,7 @@ public class CoreService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
     @Override
